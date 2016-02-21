e7f03bb2ff7e30a660a57a8adeb7e264d560c7a1
StivaleDiAcciaio <lostivalediacciaio@gmail.com>
2016-02-21 01:12:33 +0000

    aggiunti files di deploy

---
+++ deploy-html-files.sh
@@ -0,0 +1,2 @@
+cp -a html/. /var/www/public_html
+echo copiati files html nella cartella /var/www/public_html
---
+++ deploy-nodejs-files.sh
@@ -0,0 +1,3 @@
+cp -a nodejs-files/. /var/www/nodejs-server/printnetwork/
+echo copiati nodejs-files nella cartella /var/www/nodejs-server/printnetwork
+
