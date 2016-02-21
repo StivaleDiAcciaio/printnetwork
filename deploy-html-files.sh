705c831f600c460ec5a00144242207e06370a3b2
StivaleDiAcciaio <lostivalediacciaio@gmail.com>
2016-02-21 01:40:36 +0000

    Corretto file di deploy html

--- deploy-html-files.sh
+++ deploy-html-files.sh
@@ -1,2 +1,2 @@
-cp -a html/. /var/www/public_html
-echo copiati files html nella cartella /var/www/public_html
+cp -a html/. /var/www/public_html/printnetwork
+echo copiati files html nella cartella /var/www/public_html/printnetwork
