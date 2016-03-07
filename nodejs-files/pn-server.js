55f14235b3cf2e1e022f57cb92886977bd4e4d29
StivaleDiAcciaio <lostivalediacciaio@gmail.com>
2016-03-07 00:03:47 +0000

    Aggiunto file di configurazione applicazione nodejs utilizzo di jsonwebtoke

--- nodejs-files/pn-server.js
+++ nodejs-files/pn-server.js
@@ -1,20 +1,32 @@
-var express     = require('express');
+var express = require('express');
 var app = express();
-var appRouter   = express.Router();
+var appRouter = express.Router();
 var porta = 3000;
+var config = require('./config'); // mio file di configurazione
+var jwt    = require('jsonwebtoken'); // usato per creare e verificare tokens
+app.set('superSecret', config.secret); // parola segreta per generazione token
 
-appRouter.get('/', function(req, res) {
-    res.json({ data: 'PrintNetwork HOME'});
+appRouter.get('/', function (req, res) {
+    res.json({data: 'PrintNetwork HOME'});
 });
 
-appRouter.get('/2D', function(req, res) {
-   res.json({ data: 'stampa 2D avviata!'});
+appRouter.get('/2D', function (req, res) {
+    res.json({data: 'stampa 2D avviata!'});
 });
 
-appRouter.get('/3D', function(req, res) {
-   res.json({ data: 'stampa 3D avviata!'});
+appRouter.get('/3D', function (req, res) {
+    var user ={
+        nome : 'sasa'
+    };
+    var token = jwt.sign(user, app.get('superSecret'), {
+        expiresIn: '5h'
+    });
+    res.json({data: 'stampa 3D avviata!',
+              token: token
+             }
+    );
 });
 
 app.use('/apinode', appRouter);
 app.listen(3000);
-console.log('nodJs server in ascolto sulla porta '+porta );
+console.log('nodJs server in ascolto sulla porta ' + porta);
