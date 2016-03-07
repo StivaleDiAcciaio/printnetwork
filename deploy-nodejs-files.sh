dc3dd4faa350cbecddca7455921551f1414b7649
StivaleDiAcciaio <lostivalediacciaio@gmail.com>
2016-03-07 00:11:27 +0000

    modificato il file di deploy del nodejsserver

--- deploy-nodejs-files.sh
+++ deploy-nodejs-files.sh
@@ -1,4 +1,6 @@
 cp -a nodejs-files/package.json  /var/www/nodejs-server/printnetwork/
 echo copiato package.json nella cartella /var/www/nodejs-server/printnetwork
 cp -a nodejs-files/pn-server.js  /var/www/nodejs-server/printnetwork/ 
-echo pn-server.js nella cartella /var/www/nodejs-server/printnetwork
+echo copiato pn-server.js nella cartella /var/www/nodejs-server/printnetwork
+cp -a nodejs-files/config.js /var/www/nodejs-server/printnetwork
+echo copiato config.js nella cartella /var/www/nodejs-server/printnetwork
