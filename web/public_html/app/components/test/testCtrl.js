beb540476d3f7497fa68f396e99887ba87cc5907
StivaleDiAcciaio <lostivalediacciaio@gmail.com>
2016-04-16 10:12:29 +0000

    Eliminata cartella di test

--- web/public_html/app/components/test/testCtrl.js
+++
@@ -1,9 +0,0 @@
-;
-(function () {
-    'use strict';
-    angular.module('printNetworkApp').controller('testAppCtrl',
-            ['$scope', 'apiService',
-                function ($scope, api) {
-                    console.log("testapp controller ");
-                }]);
-}());
