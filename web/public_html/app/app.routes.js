21d688b80fa3e9a9b97fecd01c292a5f80ced8d2
StivaleDiAcciaio <lostivalediacciaio@gmail.com>
2016-04-17 14:14:37 +0000

    Aggiunta pagina di login

--- web/public_html/app/app.routes.js
+++ web/public_html/app/app.routes.js
@@ -3,7 +3,6 @@
     'use strict';
     angular.module('printNetworkApp')
             .config(function ($stateProvider, $urlRouterProvider) {
-
                 $stateProvider
                         .state('home', {
                             url: '/',
@@ -18,6 +17,14 @@
                             views: {'content@': {
                                     templateUrl: 'app/components/registrazione/registrazione.html',
                                     controller: 'registrazioneCtrl'
+                                }
+                            }
+                        })
+                        .state('login', {
+                            url: '/login',
+                            views: {'content@': {
+                                    templateUrl: 'app/components/login/login.html',
+                                    controller: 'loginCtrl'
                                 }
                             }
                         });
---
+++ web/public_html/app/components/login/loginCtrl.js
@@ -0,0 +1,24 @@
+;
+(function () {
+    'use strict';
+    angular.module('printNetworkApp').controller('loginCtrl',
+            ['$scope', '$http', 'apiService',
+                function ($scope, $http, serviziCustom) {
+                    console.log("loginCtrl controller ");
+                    $scope.outRestCall = "";
+                    $scope.outRestCallPOST = "";
+
+                    $scope.cliccaLogin = function () {
+
+                        var url = 'http://localhost/printnetwork/apinode/login';
+                        var myCall = $http({
+                            method: 'GET',
+                            url: url,
+                            headers: {'Content-Type': 'application/json'}
+                        });
+                        myCall.then(function (response) {
+                            $scope.outRestCall = response.data;
+                        });
+                    };
+                }]);
+}());
