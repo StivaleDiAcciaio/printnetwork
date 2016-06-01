6f99f2e5bcb9523ea5d7ee5eb4bf32d26411496e
stivale <stivale@Stivale-PC>
2016-06-01 23:59:03 +0200

    Cancellato vecchio progetto WEB/public_html

--- web/public_html/app/components/home/homeCtrl.js
+++
@@ -1,9 +0,0 @@
-;
-(function () {
-    'use strict';
-    angular.module('printNetworkApp').controller('homeCtrl',
-            ['$scope', '$state', 'serviziRest', 'CONST',
-                function ($scope, $state, serviziRest, COSTANTI) {
-
-                }]);
-}());
