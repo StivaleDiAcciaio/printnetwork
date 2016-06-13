0b23aac5c6d0f1e8463b83188ca7e4cc7477db8f
stivale <stivale@Stivale-PC>
2016-06-14 00:19:40 +0200

    revisione

--- PnFe/public_html/app/components/autenticazione/autenticazioneCtrl.js
+++ PnFe/public_html/app/components/autenticazione/autenticazioneCtrl.js
@@ -42,8 +42,14 @@
 
                     $scope.registrazione = function (formRegistrazione) {
                         if (formRegistrazione.$valid) {
-                            if ($scope.formatiStampa2DScelti) {
-                                $scope.formRegistrazioneData.tipologiaStampa.stampa2D.formato = $scope.formatiStampa2DScelti;
+                            if ($scope.condividoStampante){
+                                if ($scope.formatiStampa2DScelti) {
+                                    $scope.formRegistrazioneData.tipologiaStampa.stampa2D.formato = $scope.formatiStampa2DScelti;
+                                }
+
+                            }else{
+                                $scope.formRegistrazioneData.tipologiaStampa = null;
+                                $scope.formRegistrazioneData.indirizzo = null;
                             }
                             serviziRest.registrazione({utente: $scope.formRegistrazioneData}).then(function (response) {
                                 if (response.esito) {
