73f2ca28493795f09412a1cd15b7943d503fe3cf
stivale <stivale@Stivale-PC>
2016-06-16 23:33:52 +0200

    revisione

--- PnBe/config.js
+++ PnBe/config.js
@@ -1,4 +1,9 @@
+/* su Pc-Desktop ho installato una
+ * versione diversa di MongoDB che ha
+ * una url di connessione diversa da quella in Vbox
+ * */
 module.exports = {
     'secret': 'lostivalediacciaio',
-    'database':'mongodb://pnetworkDbAdmin:4a74s2@localhost:27017/printNetworkDb?3t.connection.name=pnetworkDB'
+    'database':'mongodb://pnetworkDbAdmin:4a74s2@localhost:27017/pnetworkDB?3t.uriVersion=2&3t.connectionMode=direct&readPreference=primary&3t.connection.name=pnetworkDB',
+    'databaseVB':'mongodb://pnetworkDbAdmin:4a74s2@localhost:27017/printNetworkDb?3t.connection.name=pnetworkDB'
 };
