5643a21f7c1d021c37c74373851dc6da26851ce1
StivaleDiAcciaio <lostivalediacciaio@gmail.com>
2016-07-08 18:20:17 +0000

    aggiunto reCAPTCHA di google

--- PnBe/config.js
+++ PnBe/config.js
@@ -4,8 +4,8 @@
  * */
 module.exports = {
     'secret': 'lostivalediacciaio',
-    'database':'mongodb://pnetworkDbAdmin:4a74s2@localhost:27017/pnetworkDB?3t.uriVersion=2&3t.connectionMode=direct&readPreference=primary&3t.connection.name=pnetworkDB',
-    'databaseVB':'mongodb://pnetworkDbAdmin:4a74s2@localhost:27017/printNetworkDb?3t.connection.name=pnetworkDB',
-    'captchaSecretKey':'secretKeyGoogle',
+    'database':'mongodb://pnetworkDbAdmin:xxxxxx@localhost:27017/pnetworkDB?3t.uriVersion=2&3t.connectionMode=direct&readPreference=primary&3t.connection.name=pnetworkDB',
+    'databaseVB':'mongodb://pnetworkDbAdmin:xxxxxx@localhost:27017/printNetworkDb?3t.connection.name=pnetworkDB',
+    'captchaSecretKey':'6LcciCQTAAAAAOcjTtecBkPsLoQtAVuN7J0AIJGe',
     'googleVerifyCaptcha':'https://www.google.com/recaptcha/api/siteverify'
 };
