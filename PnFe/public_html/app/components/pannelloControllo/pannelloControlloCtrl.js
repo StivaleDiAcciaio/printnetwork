fa35604632704d65bd0d26120c8551132a07b3ea
StivaleDiAcciaio <lostivalediacciaio@gmail.com>
2017-06-11 22:57:49 +0200

    aggiunto autocompletamento indirizzo

--- PnFe/public_html/app/components/pannelloControllo/pannelloControlloCtrl.js
+++ PnFe/public_html/app/components/pannelloControllo/pannelloControlloCtrl.js
@@ -2,8 +2,16 @@
 (function () {
     'use strict';
     angular.module('printNetworkApp').controller('pannelloControlloCtrl',
-            ['$scope', '$state','serviziRest','CONST',
-                function ($scope, $state,serviziRest,COSTANTI) {
-                   $scope.isCollapsedHorizontal = true;
+            ['$scope', '$state', 'serviziRest', 'CONST','NgMap',
+                function ($scope, $state, serviziRest, COSTANTI,NgMap) {
+                    $scope.isCollapsedHorizontal = true;
+                    $scope.map=null;
+                    $scope.placeChanged = function () {
+                        $scope.place = this.getPlace();
+                        $scope.map.setCenter($scope.place.geometry.location);
+                    };
+                    NgMap.getMap().then(function (map) {
+                        $scope.map = map;
+                    });
                 }]);
 }());
