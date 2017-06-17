8c747d869bbca2266da6a864832f851823958374
StivaleDiAcciaio <lostivalediacciaio@gmail.com>
2017-06-18 01:05:36 +0200

    migliorie alla ricerca indirizzo

--- PnFe/public_html/app/components/pannelloControllo/pannelloControlloCtrl.js
+++ PnFe/public_html/app/components/pannelloControllo/pannelloControlloCtrl.js
@@ -2,16 +2,33 @@
 (function () {
     'use strict';
     angular.module('printNetworkApp').controller('pannelloControlloCtrl',
-            ['$scope', '$state', 'serviziRest', 'CONST','NgMap',
-                function ($scope, $state, serviziRest, COSTANTI,NgMap) {
+            ['$scope', '$state', 'serviziRest', 'CONST', 'NgMap',
+                function ($scope, $state, serviziRest, COSTANTI, NgMap) {
                     $scope.isCollapsedHorizontal = true;
-                    $scope.map=null;
-                    $scope.placeChanged = function () {
+                    $scope.map = null;
+                    $scope.indirizzoTrovato = null;
+                    $scope.locationTrovata = null;
+
+                    $scope.indirizzoSelezionato = function () {
                         $scope.place = this.getPlace();
-                        $scope.map.setCenter($scope.place.geometry.location);
+                        if ($scope.place.geometry) {
+                            $scope.locationTrovata = $scope.place.geometry.location;
+                            $scope.map.setCenter($scope.place.geometry.location);
+                            $scope.indirizzoTrovato = $scope.place.formatted_address;
+                        }
                     };
                     NgMap.getMap().then(function (map) {
                         $scope.map = map;
                     });
+
+                    $scope.centraMappa = function (location) {
+                        $scope.map.setCenter(location);
+                    };
+
+                    $scope.resetCercaIndirizzo = function () {
+                        $scope.locationTrovata = null;
+                        $scope.indirizzoTrovato = null;
+                        $scope.indirizzo.attuale = null;
+                    };
                 }]);
 }());
